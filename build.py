a997516a7e33623be9a25d85d5faf9ee48603fb1
Matt Marshall <communications@mrshll.uk>
2021-02-06 10:32:18 +0000

    Updated build.py from downstream contribution

--- build.py
+++ build.py
@@ -28,7 +28,7 @@ def buildThesis(format):
     filename = "thesis.{}".format(format)
 
     # define args now
-    args = ["pandoc", "src/index.md"]
+    args = ["pandoc", "src/index.md", "src/frontmatter.md"]
 
 
     # Add chapters dynamically based on a pattern, count the chapters and them add in sequence based on number (not on filename order in the list)
@@ -193,10 +193,7 @@ def main(argv):
     chapters_parser.add_argument("format", type=str, help="The format you want to build into e.g. html or docx")
 
     # Parser for building an entire website
-    website_parser = subparsers.add_parser("website", help="Generates a website including a coversheet and html, docx, and odt versions of each chapter and entire thesis")
-
-    # Parser for building the coversheet only
-    coversheet_parser = subparsers.add_parser("coversheet", help="Generates only the html coversheet for the website, useful for debugging")
+    website_parser = subparsers.add_parser("website", help="Generates a website including a frontpage and html, docx, and odt versions of each chapter and entire thesis")
 
     # Run
     args = parser.parse_args()
@@ -216,10 +213,6 @@ def markdown_thesis(args):
     elif args.subparser == "website":
         print ("Building website \n======\n")
         buildWebsite()
-
-    elif args.subparser == "coversheet":
-        print("Building coversheet \n======\n")
-        buildCoversheet(WEBSITE_PATH)
 
     elif args.subparser == "chapter":
         print("Building Chapter in {}\n======\n".format(args.format))
